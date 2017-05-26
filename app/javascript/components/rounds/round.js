c93c40c8e77152195534b4e7feb95fdac2d1eace
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2017-05-26 12:55:30 +1000

    Re-align round tables

--- app/javascript/components/rounds/round.js
+++ app/javascript/components/rounds/round.js
@@ -5,11 +5,9 @@ export default class Round extends React.Component {
 
   render () {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-offset-3 col-md-6'>
-            < FixtureGroups fixtureGroups={this.props.fixtures} />
-          </div>
+      <div className='row'>
+        <div className='col-md-offset-3 col-md-6'>
+          < FixtureGroups fixtureGroups={this.props.fixtures} />
         </div>
       </div>
     );
