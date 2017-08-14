bd4bc3ced180e1f6e7bea422ad018568a1e59d44
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2017-08-14 21:17:53 +1000

    Add player past histories to worker

--- app/workers/recurring_player_fixture_histories_worker.rb
+++ app/workers/recurring_player_fixture_histories_worker.rb
@@ -8,12 +8,14 @@ class RecurringPlayerFixtureHistoriesWorker
 
   def perform
     Player.all.each do |player|
-      pfhs = HTTParty.get("https://fantasy.premierleague.com/drf/element-summary/#{player.id}")['history']
-                     .select { |pfh| pfh['minutes'] > 0 }
-      next unless pfhs
-      player.update(player_fixture_histories: pfhs)
+      player_json = HTTParty.get("https://fantasy.premierleague.com/drf/element-summary/#{player.id}")
+      player.update(player_past_histories: player_json['history_past'])
+      fixture_histories = player_json['history'].select { |pfh| pfh['minutes'] > 0 }
+      next unless fixture_histories
+      player.update(player_fixture_histories: fixture_histories)
+
       player_stats_arr.each do |stat|
-        player.update(stat =>  pfhs.inject(0) { |sum, pfh| sum + pfh[stat] })
+        player.update(stat =>  fixture_histories.inject(0) { |sum, pfh| sum + pfh[stat] })
       end
     end
   end
