9728d6619f459181ae90da0978ae857ca754c77c
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2017-08-28 08:55:20 +1000

    Update process_ranking_service logic

--- app/services/leagues/process_ranking_service.rb
+++ app/services/leagues/process_ranking_service.rb
@@ -8,6 +8,7 @@ class Leagues::ProcessRankingService < ActiveInteraction::Base
     end
     league.fpl_teams.order(total_score: :desc).each_with_index do |fpl_team, i|
       fpl_team.update(rank: i + 1)
+      fpl_team.fpl_team_lists.find_by(round: round).update(overall_rank: fpl_team.rank)
     end
   end
 end
