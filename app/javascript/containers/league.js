3da7618f707b7f156c9e3249a64b727b35424baf
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2017-12-19 22:02:08 +1100

    Add round count down to league view

--- app/javascript/containers/league.js
+++ app/javascript/containers/league.js
@@ -5,12 +5,16 @@ import { Provider } from 'react-redux';
 import { Panel, Accordion, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Icon } from 'react-fa';
+import axios from 'axios';
+
 import fetchLeague from '../actions/action_fetch_league.js';
 import fetchDraftPicks from '../actions/action_fetch_draft_picks.js';
 import createDraftPicks from '../actions/action_create_draft_picks.js';
+import fetchCurrentRound from '../actions/action_fetch_current_round.js';
+
 import FplTeamsTable from '../components/leagues/fpl_teams_table.js';
 import TeamListsTable from '../components/leagues/team_lists_table.js';
-import axios from 'axios';
+import RoundCountdown from '../components/rounds/round_countdown.js';
 
 class League extends Component {
   constructor(props) {
@@ -19,8 +23,10 @@ class League extends Component {
       disabled: false
     }
 
+    this.props.fetchCurrentRound();
     this.createDraftPicksButton = this.createDraftPicksButton.bind(this);
     this.createDraftPicks = this.createDraftPicks.bind(this);
+    this.roundCountdown = this.roundCountdown.bind(this);
   }
 
   componentWillMount () {
@@ -38,7 +44,10 @@ class League extends Component {
       current_user: nextProps.current_user,
       draft_picks: nextProps.draft_picks,
       fpl_team_list_arr: nextProps.fpl_team_list_arr,
-      fpl_team: nextProps.fpl_team
+      fpl_team: nextProps.fpl_team,
+      current_round: nextProps.current_round,
+      current_round_deadline: nextProps.current_round_deadline,
+      current_round_status: nextProps.current_round_status
     })
   }
 
@@ -64,6 +73,18 @@ class League extends Component {
     this.props.createDraftPicks(leagueId)
   }
 
+  roundCountdown () {
+    if (this.state.current_round_deadline) {
+      return (
+        <RoundCountdown
+          round={ this.state.current_round }
+          current_round_deadline={ this.state.current_round_deadline }
+          current_round_status={ this.state.current_round_status }
+        />
+      );
+    }
+  }
+
   render () {
     if (this.state == null || this.state.league == null || this.state.draft_picks == null) {
       return (
@@ -72,6 +93,7 @@ class League extends Component {
     } else {
       return (
         <div>
+          { this.roundCountdown() }
           <h2>{ this.state.league.name }</h2>
           <p>Commissioner: { this.state.commissioner.username }</p>
           <FplTeamsTable
@@ -94,7 +116,10 @@ function mapStateToProps(state) {
     fpl_team_list_arr: state.LeagueReducer.fpl_team_list_arr,
     commissioner: state.LeagueReducer.commissioner,
     current_user: state.LeagueReducer.current_user,
-    draft_picks: state.DraftPicksReducer.draft_picks
+    draft_picks: state.DraftPicksReducer.draft_picks,
+    current_round: state.CurrentRoundReducer.current_round,
+    current_round_deadline: state.CurrentRoundReducer.current_round_deadline,
+    current_round_status: state.CurrentRoundReducer.current_round_status,
   }
 }
 
@@ -102,7 +127,8 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({
     fetchLeague: fetchLeague,
     fetchDraftPicks: fetchDraftPicks,
-    createDraftPicks: createDraftPicks
+    createDraftPicks: createDraftPicks,
+    fetchCurrentRound: fetchCurrentRound
   }, dispatch);
 }
 
